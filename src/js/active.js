3f3db4a51eff5d93208dab4e5a492a3844003e8d
Joo-Myungjin <stepersjmj@nate.com>
2019-02-07 00:34:33 +0900

    js수정

--- src/js/active.js
+++ src/js/active.js
@@ -21,6 +21,44 @@ $(document).ready(function(e) {
 		self.addClass('on').siblings().removeClass('on');
 	}
 
+	//selectbox
+	$('.cmm_select').each(function () {
+	    var $this = $(this),
+	        numberOfOptions = $(this).children('option').length;
+	    $this.addClass('s-hidden');
+	    $this.wrap('<div class="select"></div>');
+	    $this.after('<div class="styledSelect"></div>');
+	    var $styledSelect = $this.next('div.styledSelect');
+	    $styledSelect.text($this.children('option').eq(0).text());
+	    var $list = $('<ul />', {
+	        'class': 'options'
+	    }).insertAfter($styledSelect);
+	    for (var i = 0; i < numberOfOptions; i++) {
+	        $('<li />', {
+	            text: $this.children('option').eq(i).text(),
+	            rel: $this.children('option').eq(i).val()
+	        }).appendTo($list);
+	    }
+	    var $listItems = $list.children('li');
+	    $styledSelect.click(function (e) {
+	        e.stopPropagation();
+	        $('div.styledSelect.active').each(function () {
+	            $(this).removeClass('active').next('ul.options').hide();
+	        });
+	        $(this).toggleClass('active').next('ul.options').toggle();
+	    });
+	    $listItems.click(function (e) {
+	        e.stopPropagation();
+	        $styledSelect.text($(this).text()).removeClass('active');
+	        $this.val($(this).attr('rel'));
+	        $list.hide();
+	    });
+	    $(document).click(function () {
+	        $styledSelect.removeClass('active');
+	        $list.hide();
+	    });
+	});
+
 	//dropdown
 	$('.dropdown .btn_dropdown').on('click',function(e){
 		var self = $(this);
@@ -74,7 +112,7 @@ $(document).ready(function(e) {
 		},
 		function() {
 		  slideUp();
-		},
+		}
 	);
 	$('.lnb').hover(
 		function() {
